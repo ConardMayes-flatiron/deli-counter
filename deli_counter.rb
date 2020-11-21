8db58f3cce504b66f4bb687f895f7e8e8f617ae1
ConardMayes-flatiron <ConradAsatoMayes+flatironschool@gmail.com>
2020-11-21 00:48:58 -0600

    commit tree 8f78af104e169ce895f8e51a1fe9c0499ac4e7fc

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+require "pry"
+
+# test arrays, comment when not in use
+array = ["steve", "bert", "sally"]
+real_array = ["bazil", "duncan", "wyoming"]
+
+
+katz_deli =[]
+
+def line(queue)
+    if queue.length == 0
+        puts "The line is currently empty."
+    else
+        anonunce = ["The line is currently:"]
+        queue.each do |person|
+             anonunce.push(" #{queue.index("#{person}")+1}. #{person}")
+        end
+        puts anonunce.join("")
+    end
+end
+
+def take_a_number(queue, person)
+    katz_deli = queue.append(person)
+    puts "Welcome, #{person}. You are number #{queue.index("#{person}")+1} in line."
+end
+
+def now_serving(queue)
+    if queue.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{queue.shift}."
+    end
+end
+# binding.pry
